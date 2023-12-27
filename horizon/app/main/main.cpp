199860a731b21f10dd92767d0257ef45858a6280
Bozhidar Dimov <BADimov21@codingburgas.bg>
2023-12-27 16:34:33 +0200

    Set up development environment and link additional librarie

---
+++ horizon/app/main/main.cpp
@@ -0,0 +1,21 @@
+﻿#include "raylib.h"
+
+int main() {
+    const int screenWidth = 1920;
+    const int screenHeight = 975;
+
+    InitWindow(screenWidth, screenHeight, "Horizon");
+
+    SetTargetFPS(60);
+
+    // Main game loop
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        EndDrawing();
+    }
+    CloseWindow();
+    return 0;
+}
